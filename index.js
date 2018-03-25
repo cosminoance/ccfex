eec6d85be5aae11d9e931eb302b9c0ec2cd44ed5
Cosmino <cosmin_oance@yahoo.com>
2018-03-25 16:02:17 +0300

    serving index

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var http = require('http');
-http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
+    if(req.url == "/") {
+        var fs = require("fs");
+            fs.readFile("www/index.html", function(err, text){
+              res.setHeader("Content-Type", "text/html");
+              res.end(text);
+            });
+            return;
+        
+          }
+          else if (req.url == "/server"){
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
+  return;
+          }
+          else{
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(`You have requested a page ${req.url} that does not exist`); 
+          }
 }).listen(3000, 'localhost');
 console.log('Server running at http://localhost:3000/');
